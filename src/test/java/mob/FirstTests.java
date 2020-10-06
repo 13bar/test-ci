82b189959fda27cde577013aa55a0c5ee73d6a46
a.parasych <a.parasych@eapteka.ru>
2020-10-06 14:14:20 +0300

    init

---
+++ src/test/java/mob/FirstTests.java
@@ -0,0 +1,27 @@
+package mob;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FirstTests {
+
+    @Test
+    @DisplayName("Test one in SecondTests")
+    void testOne() {}
+
+    @Test
+    @DisplayName("Test one in SecondTests")
+    void testTwo() {}
+
+    @Test
+    @DisplayName("Test one in SecondTests")
+    void testThree() {}
+
+    @Test
+    @DisplayName("Test one in SecondTests")
+    void testFour() {}
+
+    @Test
+    @DisplayName("Test one in SecondTests")
+    void testFive() {}
+}
---
+++ src/test/java/mob/SecondTests.java
@@ -0,0 +1,19 @@
+package mob;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SecondTests {
+
+    @Test
+    @DisplayName("Test one in SecondTests")
+    void testOne() {}
+
+    @Test
+    @DisplayName("Test two in SecondTests")
+    void testTwo() {}
+
+    @Test
+    @DisplayName("Test three in SecondTests")
+    void testThree() {}
+}
