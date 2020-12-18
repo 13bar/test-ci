d8da494ae2d463416a6941199ef5ab91e89fa25c
a.parasych <a.parasych@eapteka.ru>
2020-12-18 17:38:08 +0300

    Increment

--- src/test/java/mob/SecondTests.java
+++ src/test/java/mob/SecondTests.java
@@ -1,8 +1,10 @@
 package mob;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@Tag("SecondTests")
 class SecondTests {
 
     @Test
